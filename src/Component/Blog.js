8f22467e5a0505ee6c27b2e728bb9b55cb5158ab
tawhidhaider <tawhidalam69@gmail.com>
2022-10-26 01:53:27 +0600

    faq& blog updated

--- src/Component/Blog.js
+++ src/Component/Blog.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            this is blog
-        </div>
+        <section className="dark:bg-gray-800 dark:text-gray-100">
+	<div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+		<h2 className="text-2xl font-semibold sm:text-4xl">Bonus Questions</h2>
+		<p className="mt-4 mb-8 dark:text-gray-400">Lets See</p>
+		<div className="space-y-4">
+			<details className="w-full border rounded-lg">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">what is cors?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400"> </p>
+			</details>
+			<details className="w-full border rounded-lg">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">Why are you using firebase? What other options do you have to implement authentication?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400"> </p>
+			</details>
+			<details className="w-full border rounded-lg">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">How does the private route work?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400"> </p>
+			</details>
+            <details className="w-full border rounded-lg">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">What is Node? How does Node work?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400"> </p>
+			</details>
+		</div>
+	</div>
+</section>
     );
 };
 
--- src/SharedComponents/Header.js
+++ src/SharedComponents/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 import Brand1 from '../Assets/Brand1.png'
 
@@ -23,15 +24,15 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                        <Nav.Link href="#pricing" className='text-white'>Envoy-learning</Nav.Link>
+                        <Nav.Link href=""><Link to={'/'} className='text-white text-2xl no-underline'>Envoy-learning</Link></Nav.Link>
                             
                             
                         </Nav>
                         <Nav>
-                        <Nav.Link href="#features" className='text-white'>Courses</Nav.Link>
+                        <Nav.Link href=""><Link to={"/course"} className='text-white no-underline'>Courses</Link></Nav.Link>
                             <Nav>
-                            <Nav.Link href="#pricing" className='text-white'>Blog</Nav.Link>
-                            <Nav.Link href="#pricing" className='text-white'>FAQ</Nav.Link>
+                            <Nav.Link href="#"><Link to={"/blog"} className='text-white no-underline'>Blog</Link></Nav.Link>
+                            <Nav.Link href="#"><Link to={'/faq'} className='text-white no-underline'>FAQ </Link> </Nav.Link>
                             <Nav.Link href="#features" className='text-white'>ToggoleTheme</Nav.Link>
                             <Nav.Link href="#deets" className='text-white'>DisplayName</Nav.Link>
                             </Nav>
